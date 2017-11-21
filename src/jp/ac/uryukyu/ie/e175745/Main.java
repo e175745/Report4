f63877c17ce94cea59ff3471d23eb7f08e4dd449
Kazuma M <e175745@ie.u-ryukyu.ac.jp>
2017-11-21 09:19:16 +0900

    課題

--- src/jp/ac/uryukyu/ie/e175745/Main.java
+++ src/jp/ac/uryukyu/ie/e175745/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        }
+        catch(NullPointerException e){
+            System.out.println("Catch NullPointerException");
+            System.out.println(e.getMessage());
+        }
     }
 }
